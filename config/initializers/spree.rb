1a78009b7e74298fc94eb964e56e1ae115e12698
Huy Dinh <dinhhuydh@gmail.com>
2013-02-19 21:49:58 +0700

    change s3 bucket

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -14,7 +14,7 @@ Spree.config do |config|
   config.admin_interface_logo = 'admin/admin_logo.jpg'
 
   config.use_s3 = true
-  config.s3_bucket = 'vietcare24h'
+  config.s3_bucket = 'dh_vietcare24h'
   config.s3_access_key = ENV['S3_ACCESS_KEY']
   config.s3_secret = ENV['S3_SECRET'] 
 end
